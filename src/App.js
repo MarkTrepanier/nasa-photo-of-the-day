6c13dde0e7ed85d1d51feebc949979aa82126c16
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-09 16:06:49 -0400

    added styles ^^

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React,{useState, useEffect} from "react";
 import "./App.css";
 import Axios from 'axios';
 import Card from "./card";
+import styled from "styled-components";
 
+const AppStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color:${pr=>pr.theme.black};
+  h1{
+    color:${pr=>pr.theme.secondaryColor};
+    background-color: ${pr=>pr.theme.primaryColor};
+    border: thick ${pr=>pr.theme.secondaryColor} solid;
+    border-radius: 15px;
+  }
+`
 function App() {
   const [data, setData] = useState([])
 
@@ -18,12 +31,12 @@ function App() {
 
   //return <html to vdom>
   return (
-    <div className="App">
+    <AppStyle className="App">
       <h1>NASA Photo OF The Day</h1>
       {data.length === 0 ? <p>loading</p>:
         <Card data={data}/>
       }
-    </div>
+    </AppStyle>
   );
 }
 
